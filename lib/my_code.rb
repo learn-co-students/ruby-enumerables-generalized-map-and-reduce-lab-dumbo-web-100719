95053b9c96e0459c497c7d5e522400fbb09d1731
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-21 21:49:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map (source_array, block)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(source_array[i]*#{block})
+    i+=1
+  end
+  yield return new
+end 
+
+map(source_array){|n| n * -1}
+
+map(source_array){|n| n * 1}
+
+map(source_array){|n| n * 2}
+
+map(source_array{|n| n ** 2}
+
