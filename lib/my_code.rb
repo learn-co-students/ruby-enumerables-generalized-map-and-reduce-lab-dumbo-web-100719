83e98c8ec133e99c4d25da492c4e739d6503d80e
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-22 23:27:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,58 @@
-# Your Code Here
+require 'pry'
+
+def map(array)
+  new_array = []
+  i = 0
+
+  while i < array.length
+    # p "array value is #{array[i]}"
+    new_array << yield(array[i])
+    i += 1
+  end
+
+  # for i in array.length
+  #   p "array value is #{array[i]}"
+  #   new_array.push(yield(array[i]))
+  #   p new_array[i]
+  # end
+
+  return new_array
+end
+
+
+
+# def map(array)
+#   new_array = []
+
+# # p "array is #{array}"
+
+#   array.each do |i|
+#     new_array.push(yield(array[i]))
+#     p i
+#   end
+
+#   return new_array
+# end
+
+
+
+def reduce(array, val = nil)
+  
+  i = 0
+  # bool = false
+  val ? (sum = val) : (sum = array[0]; i = 1)
+  
+  while i < array.length
+    sum = yield(sum, array[i])
+    # array[i] == true ? (bool = true) : ('') 
+    i += 1
+  end
+  
+  return sum
+  # binding.pry
+  
+  # bool == true ? (return bool) : (return sum)
+  
+end
+
+
