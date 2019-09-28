927262cb493447604d5dfba473b168bcb036b31f
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-28 20:08:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,59 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length
+   new << yield(array[i])
+   i += 1 
+  end
+  new
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0 
+  else
+    sum = array[0]
+    i = 1 
+  end
+  while i < array.length
+   sum = yield(sum, array[i])
+   i += 1
+  end
+  sum
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#block parameter - "parameter" for block
+
+#block parameter - "parameter" for block
+
+# block looks like do...end
+
+# [1,2,3].map do |num|
+#   num + 3
+  
+#   #|num| represents the data we're working with
+  
+#   def add_numbers(num1,num2)
+#     total =num1+num2
+#   end
+#   add_numbers(1,2) do |tot| 
+#     puts "The total is #{tot}"
+#   end
+
+
