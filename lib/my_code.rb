b47dbccd0ffd5f03ee7b620398ae9d8cf3e1ac5a
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-28 15:47:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(source_array)
+  
+final_array = []
+i = 0 
+  while i < source_array.size do 
+     final_array.push(yield(source_array[i]))
+     i += 1 
+   end
+   final_array
+end
+
+def reduce(source_array, starting_point = nil)
+ if starting_point
+   total = starting_point
+   i = 0 
+ else
+  total = source_array[0]
+  i = 1
+end
+
+  while i < source_array.size do 
+    total = yield(total, source_array[i])
+    i += 1
+   end
+   total
+end
