7e82319b0a84c656241bfa795fe35c4c6cb3457a
Diego Martinez <dmartinezm@gmail.com>
2019-09-30 01:32:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+
+def map(source_array)
+  noo_array=[]
+  source_array.each do |elem|
+   noo_array << yield(elem)
+  end
+  noo_array
+end
+
+def reduce(source_array, starting_point=nil, &block)
+  #total= starting_point
+  
+  #source_array.each do |elem|
+#    total= block.call(total,elem)
+#  end
+ #total
+if starting_point.nil?
+   source_array.reduce(&block)
+  else
+   source_array.reduce(starting_point, &block)
+ end
+end
