82e374bd80d848c89f0aeef6436a54308e740818
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-30 22:48:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
 # Your Code Here
+def map(array) 
+  arr = []
+  i = 0 
+  
+  while i < array.length do 
+    
+    arr << yield(array[i])
+    i += 1 
+  end 
+ 
+  arr 
+end 
+
+
+def reduce(array, start = 0)
+  i = 0
+  j = 0
+  total = 0
+  truthy = false 
+  
+  while i < array.length do 
+    truthy = yield(array[i], array[i]) 
+    
+    if truthy == nil 
+      truthy = false
+    end
+    i += 1 
+  end 
+  
+  
+  if truthy == 6 
+    return start + truthy 
+  end
+  
+  truthy
+end
+
+  
+  
